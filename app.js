b616f587b126762a4ac068852d1a377821ade262
Rajitha-Manellanga <rajitha.mail48@gmail.com>
2023-12-20 08:30:32 +0530

    updated port in app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 require('dotenv').config({ path: './config/.env' });
 const app = express();
-const port = 3000; 
-const p = process.env.PORT;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.use(express.static('public'));
@@ -11,6 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(p, () => {
-  console.log(`Server running on http://localhost:${p}/`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
